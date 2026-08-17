9ce46d900cc82a7e8bb89f1c2fb44acc880c9eb2
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-08-17 19:30:26 -0300

    alterando deploy do MySQL para Aiven

--- Backend/Models/data_base.py
+++ Backend/Models/data_base.py
@@ -7,15 +7,24 @@ load_dotenv()
 db = SQLAlchemy()
 
 def iniciar_db(app):
-    user     = os.getenv('DB_USER', 'root')
-    password = os.getenv('DB_PASSWORD', '')
-    host     = os.getenv('DB_HOST', 'mysql')
-    port     = os.getenv('DB_PORT', '3306')
-    name     = os.getenv('DB_NAME', 'aluguel_carros')
+    user     = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    host     = os.getenv('DB_HOST')
+    port     = os.getenv('DB_PORT')
+    name     = os.getenv('DB_NAME')
+    ssl_ca   = os.getenv('DB_SSL_CA')
 
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{name}'
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if ssl_ca:
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+            'connect_args': {
+                'ssl_ca': ssl_ca,
+                'ssl_verify_cert': True,
+            }
+        }
 
     db.init_app(app)
